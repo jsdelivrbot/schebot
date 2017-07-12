163d98bb2754a2437c2154496f3ae75ea09bc8b9
orangies <somsumsun@gmail.com>
2017-07-12 10:54:47 +0700

    first start

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var http = require('http');
 var app = express();
+var api = require('instagram-node').instagram();
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -13,8 +16,46 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+// app.configure(function() {
+//   // The usual... 
+// });
+
+
+api.use({
+  client_id: '179b7e3894764c9dad6bdce62d422949',
+  client_secret: 'ba85a46e88db41919aa22fcc175324a8 '
 });
+var redirect_uri = 'https://afternoon-coast-78677.herokuapp.com/callback';
+
+
+exports.authorize_user = function(req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+};
+ 
+exports.handleauth = function(req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+    if (err) {
+      console.log(err.body);
+      res.send("Didn't work");
+    } else {
+      console.log('Yay! Access token is ' + result.access_token);
+      res.send('You made it!!');
+    }
+  });
+};
+ 
+// This is where you would initially send users to authorize 
+app.get('/authorize_user', exports.authorize_user);
+// This is your redirect URI 
+app.get('/handleauth', exports.handleauth);
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 
