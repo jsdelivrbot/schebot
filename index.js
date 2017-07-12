ebe29c0e4a60aa13b5814a3ae0c8a16c05ddff13
orangies <somsumsun@gmail.com>
2017-07-12 15:16:20 +0700

    update privacy

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var http = require('http');
 var app = express();
 var api = require('instagram-node').instagram();
 
+var InstagramAPI = require('instagram-api');
+var accessToken = '55502361.179b7e3.aadfa417c1584a3cb64dc6c8b45816f6';//'23612221.3fcb46b.348431486f3a4fb85081d5242db9ca1c';
+var instagramAPI = new InstagramAPI(accessToken);
+
+
+const InstagramNodeApi = require('instagram-node-api');
+const instagramNodeApi = new InstagramNodeApi(accessToken);
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -12,7 +20,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   response.render('pages/index');
 });
 
@@ -29,37 +37,44 @@ api.use({
 var redirect_uri = 'https://afternoon-coast-78677.herokuapp.com/handleauth';
 
 
-exports.authorize_user = function(req, res) {
- res.redirect(api.get_authorization_url(redirect_uri, { scope: ['basic+public_content'] }));
+exports.authorize_user = function (req, res) {
+  res.redirect(api.get_authorization_url(redirect_uri, { scope: ['basic+public_content'] }));
 };
- 
-exports.handleauth = function(req, res) {
-  api.authorize_user(req.query.code, redirect_uri, function(err, result) {
+
+exports.handleauth = function (req, res) {
+  api.authorize_user(req.query.code, redirect_uri, function (err, result) {
     if (err) {
       console.log(err.body);
       res.send("denied");
     } else {
       console.log('Yay! Access token is ' + result.access_token);
-    
+
       api.use({ access_token: result.access_token });
- 
-      res.send('You made it!! access_token is ' + result.access_token );
-      }
+
+      res.send('You made it!! access_token is ' + result.access_token);
+    }
   });
 };
- 
+
 // This is where you would initially send users to authorize 
 app.get('/authorize_user', exports.authorize_user);
 // This is your redirect URI 
 app.get('/handleauth', exports.handleauth);
- 
-http.createServer(app).listen(app.get('port'), function(){
+
+http.createServer(app).listen(app.get('port'), function () {
   console.log("Express server listening on port " + app.get('port'));
 });
 
 
-app.post('/subscription', function(req, res) {
-  
+app.get('/media', function (req, res) {
+  instagramNodeApi.user('333cyj333');
+  instagramNodeApi.on('data', (profile, meta, remaining, limit, result) => {
+    console.log(profile);
+  });
+  // instagramNodeApi.usersSelf();
+  // instagramNodeApi.on('data', (profile, meta, remaining, limit, result) => {
+  //     console.log(profile);
+  // });
 });
 
 
