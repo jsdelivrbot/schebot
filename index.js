c19f7f11dbb843b797af3b6aeed4f92da4271619
orangies <somsumsun@gmail.com>
2018-05-19 14:40:00 +0700

    check story > 1

--- index.js
+++ index.js
@@ -409,76 +409,76 @@ function DoCheckMedia(username) {
                 var story_count = body.items.length;
                 //console.log("story have : " + story_count);
                 if (story_count > 0) {
-                    var item = body.items[0];
-                    //var code = item.code;
-                    //DateTime Taken
-                    var taken_at = item.taken_at;
-                    var taken_at_mm = moment.unix(taken_at);
-                    var time_taken = momentTz.tz(taken_at_mm, "Asia/Seoul").format('MMM DD YYYY, HH:mm');
-                    console.log("Taken At : " + time_taken);
-                    var time_taken_forchk = moment(taken_at_mm).format('YYYY-MM-DD HH:mm:00');
+                    for (var c = 0; c < story_count; c++) {
 
 
-                    if (time_taken_forchk == chklastMin) {
+                        var item = body.items[c];
+                        //var code = item.code;
+                        //DateTime Taken
+                        var taken_at = item.taken_at;
+                        var taken_at_mm = moment.unix(taken_at);
+                        var time_taken = momentTz.tz(taken_at_mm, "Asia/Seoul").format('MMM DD YYYY, HH:mm');
+                        console.log("Taken At : " + time_taken);
+                        var time_taken_forchk = moment(taken_at_mm).format('YYYY-MM-DD HH:mm:00');
 
-                        //CAPTION
-                        var chkcaption = item.caption;
-                        var textcaption = "";
-                        if (chkcaption != null) {
-                            textcaption = item.caption.text;
-                        }
-                        var caption0 = "[YOUNGJAE_STORY] " + textcaption;
-                        var caption1 = "\n#영재 #GOT7\n";
-                        var caption = caption0 + caption1 + time_taken;
-                        console.log(caption);
-                        //Media Type
-                        var media_type = item.media_type;
-                        if (media_type == 1) { //Picture
-                            var original_width = item.original_width;
-                            var original_height = item.original_height;
-                            var img_ver2 = item.image_versions2;
-                            var candidates_length = item.image_versions2.candidates.length;
-                            console.log(candidates_length);
-                            for (var i = 0; i < candidates_length; i++) {
-                                if (img_ver2.candidates[i].width == original_width && img_ver2.candidates[i].height == original_height) { // Maxinum,Original Image
-                                    console.log("found " + original_width, original_height);
-                                    var img_url = img_ver2.candidates[i].url;
-                                    console.log("Image URL : " + img_url);
-                                    var stream = request(img_url).pipe(fs.createWriteStream(`./public/media/${storyid}.jpg`));
-                                    stream.on('finish', function () {
-                                        console.log('---stream done---')
-                                        //POST TWITTER
-                                        console.log("start tweet image");
-                                        TweetImage(storyid, caption);
-                                    });
+
+                        if (time_taken_forchk == chklastMin) {
+
+                            //CAPTION
+                            var chkcaption = item.caption;
+                            var textcaption = "";
+                            if (chkcaption != null) {
+                                textcaption = item.caption.text;
+                            }
+                            var caption0 = "[YOUNGJAE_STORY] " + textcaption;
+                            var caption1 = "\n#영재 #GOT7\n";
+                            var caption = caption0 + caption1 + time_taken;
+                            console.log(caption);
+                            //Media Type
+                            var media_type = item.media_type;
+                            if (media_type == 1) { //Picture
+                                var original_width = item.original_width;
+                                var original_height = item.original_height;
+                                var img_ver2 = item.image_versions2;
+                                var candidates_length = item.image_versions2.candidates.length;
+                                console.log(candidates_length);
+                                for (var i = 0; i < candidates_length; i++) {
+                                    if (img_ver2.candidates[i].width == original_width && img_ver2.candidates[i].height == original_height) { // Maxinum,Original Image
+                                        console.log("found " + original_width, original_height);
+                                        var img_url = img_ver2.candidates[i].url;
+                                        console.log("Image URL : " + img_url);
+                                        var stream = request(img_url).pipe(fs.createWriteStream(`./public/media/${storyid}.jpg`));
+                                        stream.on('finish', function () {
+                                            console.log('---stream done---')
+                                            //POST TWITTER
+                                            console.log("start tweet image");
+                                            TweetImage(storyid, caption);
+                                        });
+                                    }
                                 }
+
+
                             }
 
+                            if (media_type == 2) { //Video
+                                var video_url = item.video_versions[0].url;
+                                console.log("VIDEO URL : " + video_url);
 
-                        }
+                                var stream = request(video_url).pipe(fs.createWriteStream(`./public/media/${storyid}.mp4`));
+                                stream.on('finish', function () {
+                                    console.log('---stream video done---')
 
-                        if (media_type == 2) { //Video
-                            var video_url = item.video_versions[0].url;
-                            console.log("VIDEO URL : " + video_url);
-
-                            var stream = request(video_url).pipe(fs.createWriteStream(`./public/media/${storyid}.mp4`));
-                            stream.on('finish', function () {
-                                console.log('---stream video done---')
-
-                                var videoTweet = new VideoTweet({
-                                    file_path: `./public/media/${storyid}.mp4`,
-                                    tweet_text: caption
+                                    var videoTweet = new VideoTweet({
+                                        file_path: `./public/media/${storyid}.mp4`,
+                                        tweet_text: caption
+                                    });
                                 });
-                            });
 
+
+                            }
 
                         }
-
                     }
-
-
-
-
 
                 }
             })
