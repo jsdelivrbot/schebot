460bccabfe44ef1caefdaa83a82d94ddf05078ef
orangies <somsumsun@gmail.com>
2018-06-03 17:48:32 +0700

    add config

---
+++ config/default.js
@@ -0,0 +1,58 @@
+module.exports = 
+
+{
+    "cyjname": "333cyj333",
+    "defname" : "prdsdef",
+    "session_id" : "IGSC477a4db653a8aa190755c21f3a9b79354829e639893a0e8616e8896dc82cc35e%3AZLTkdjiyZs46NGt4ecDskm2obFrZFFCt%3A%7B%22_auth_user_id%22%3A55502361%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%2255502361%3A1VkcHjcNMfYpOJzuxH538jjpztZzRpKJ%3A35bb8bca913d21bbf6e1977e18e2aa06b5ddcf3e40429aac7e490618028464db%22%2C%22last_refreshed%22%3A1524452065.9287192822%7D",      
+    "cyjid": 1946899430,
+    "somzid":55502361,
+    "hashtag_cyj" :"\n#영재 #GOT7\n",
+    "hashtag_def":"\n#JB #GOT7\n",
+    "prdsdef" : [
+        {
+            "account": "prdsdef",
+            "name" : "JB",
+            "hashtag" :"\n#JB #GOT7\n",
+            "title" : "[DEFSTAGRAM] ",
+            "title_story" : "[DEF_STORY] "
+
+        }
+    ],
+    "333cyj333":[
+        {
+            "account": "333cyj333",
+            "name" : "YOUNGJAE",
+            "hashtag" : "\n#영재 #GOT7\n",
+            "title" : "[YOUNGJAESTAGRAM] ",
+            "title_story" : "[YOUNGJAE_STORY] ",
+            "auth": {
+                "consumer_key": "XPRMyf0XEgjKlAG1msgShdiFb",
+                "consumer_secret": "cZhBVhc17vNPxYav4m7BhuYGA9aNgId8WdmokzwBl66uEfY1XD",
+                "access_token_key": "703675814127149057-AhNFjjUVgN8fR9N2b86gctEgH0I8ENa",
+                "access_token_secret": "AfKLLxRj3EUhJG2Bg6Dt6hI9oMaDNzjAey3fNpjOEMNYR"
+            }
+        }
+    ],
+    "auth_del": {
+        "consumer_key": "GxnZX3Qju1ns6POPaCK0C6cWn",
+        "consumer_secret": "FMEYX0LfZNguOIduH4nfJxyDeCPRcwAf7yI6GcL1KwvqmM9Lbp",
+        "access_token_key": "898452066791596032-ubymzyqkLEH4KzM9dGecl39Ol4Kb6mU",
+        "access_token_secret": "AoTTRVa8i5evsFakHJVlunCs188TkVQciKMKbByeJtdDT"
+    },
+    "auth_cyj": {
+        "consumer_key": "XPRMyf0XEgjKlAG1msgShdiFb",
+        "consumer_secret": "cZhBVhc17vNPxYav4m7BhuYGA9aNgId8WdmokzwBl66uEfY1XD",
+        "access_token_key": "703675814127149057-AhNFjjUVgN8fR9N2b86gctEgH0I8ENa",
+        "access_token_secret": "AfKLLxRj3EUhJG2Bg6Dt6hI9oMaDNzjAey3fNpjOEMNYR"
+    },
+    "auth_lenayk": {
+        "consumer_key": "Ixvt8NaZavlDTxPxaGqd8FgOD",
+        "consumer_secret": "Ocnm7kCPd9jOztnYe3TZAU7vSUN9qGJ4hIEYV74Z2QNoJNPzB0",
+        "access_token_key": "768051804626051077-mmA2Zz4VYpP7dPI0vsKf2sdM3tqio4F",
+        "access_token_secret": "sXoLtfKSKIf7kgrAfs2TGCbwICQeruZeOIVX2rPyLOgKP"
+    },
+    "VIDEO":{
+        "endpoint" :'https://upload.twitter.com/1.1/media/upload.json',
+        "post_tweet_url" : 'https://api.twitter.com/1.1/statuses/update.json'
+    }
+}
