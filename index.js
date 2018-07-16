51e5e896b5372338a29742dd2aa24ea53f0912e9
orangies <somsumsun@gmail.com>
2018-07-16 09:56:04 +0700

    change tweet video function

--- index.js
+++ index.js
@@ -466,10 +466,12 @@ function DoCheckMedia(username) {
                                 stream.on('finish', function () {
                                     console.log('---stream video done---')
 
-                                    var videoTweet = new VideoTweet({
-                                        file_path: `./public/media/${storyid}.mp4`,
-                                        tweet_text: caption
-                                    });
+                                    var file_path = `./public/media/${storyid}.mp4`;
+                                    TweetVideo(file_path, caption);
+                                    // var videoTweet = new VideoTweet({
+                                    //     file_path: `./public/media/${storyid}.mp4`,
+                                    //     tweet_text: caption
+                                    // });
                                 });
 
 
@@ -540,8 +542,8 @@ function CheckMediaDataType(code) {
         console.log("timestmp : " + timestmp);
         console.log("-----------------------CHECK LENGTH--------------------");
         console.log("First Text : " + fistfixedTxt.length);
-        console.log("HASHTAG + LINK : " +hashtagLink.length );
-        console.log("TIMESTAMP :  " +  timestmp.length);
+        console.log("HASHTAG + LINK : " + hashtagLink.length);
+        console.log("TIMESTAMP :  " + timestmp.length);
         var txtLeft = 180 - fistfixedTxt.length - hashtagLink.length - timestmp.length - 3;
         console.log("txtLeft : " + txtLeft);
 
@@ -593,10 +595,13 @@ function CheckMediaDataType(code) {
             stream.on('finish', function () {
                 console.log('---stream video done---')
 
-                var videoTweet = new VideoTweet({
-                    file_path: `./public/media/${code}.mp4`,
-                    tweet_text: total_msg_tweet
-                });
+                var file_path = `./public/media/${code}.mp4`;
+                TweetVideo(file_path, total_msg_tweet);
+
+                // var videoTweet = new VideoTweet({
+                //     file_path: `./public/media/${code}.mp4`,
+                //     tweet_text: total_msg_tweet
+                // });
             });
         }
 
@@ -657,37 +662,54 @@ function CheckMediaDataType(code) {
                         if (carouselURL_image.length == 0) {
                             var itemLen = carouselURL_video.length;
                             var total_msg_tweet = fistfixedTxt + igcaption + `(${idvid}/${itemLen})` + hashtagLink + timestmp;
-                            var videoTweet = new VideoTweet({
-                                file_path: `./public/media/${code}_${numVid}.mp4`,
-                                tweet_text: total_msg_tweet
-                            });
+
+                            var file_path = `./public/media/${code}_${numVid}.mp4`;
+                            TweetVideo(file_path, total_msg_tweet);
+
+
+                            // var videoTweet = new VideoTweet({
+                            //     file_path: ,
+                            //     tweet_text: total_msg_tweet
+                            // });
                         }
                         if (carouselURL_image.length > 0 && carouselURL_image.length < 5) {
                             //tweet of image has 1
                             var itemLen = carouselURL_video.length + 1;
                             var total_msg_tweet = fistfixedTxt + igcaption + `(${idvid}/${itemLen})` + hashtagLink + timestmp;
-                            var videoTweet = new VideoTweet({
-                                file_path: `./public/media/${code}_${numVid}.mp4`,
-                                tweet_text: total_msg_tweet
-                            });
+
+                            var file_path = `./public/media/${code}_${numVid}.mp4`;
+                            TweetVideo(file_path, total_msg_tweet);
+
+                            // var videoTweet = new VideoTweet({
+                            //     file_path: `./public/media/${code}_${numVid}.mp4`,
+                            //     tweet_text: total_msg_tweet
+                            // });
                         }
                         if (carouselURL_image.length >= 5 && carouselURL_image.length < 9) {
                             //tweet of video has 2
                             var itemLen = carouselURL_video.length + 2;
                             var total_msg_tweet = fistfixedTxt + igcaption + `(${idvid}/${itemLen})` + hashtagLink + timestmp;
-                            var videoTweet = new VideoTweet({
-                                file_path: `./public/media/${code}_${numVid}.mp4`,
-                                tweet_text: total_msg_tweet
-                            });
+
+                            var file_path = `./public/media/${code}_${numVid}.mp4`;
+                            TweetVideo(file_path, total_msg_tweet);
+                            // var videoTweet = new VideoTweet({
+                            //     file_path: `./public/media/${code}_${numVid}.mp4`,
+                            //     tweet_text: total_msg_tweet
+                            // });
                         }
                         if (carouselURL_image.length >= 9) {
                             //tweet of video has 3
                             var itemLen = carouselURL_video.length + 3;
                             var total_msg_tweet = fistfixedTxt + igcaption + `(${idvid}/${itemLen})` + hashtagLink + timestmp;
-                            var videoTweet = new VideoTweet({
-                                file_path: `./public/media/${code}_${numVid}.mp4`,
-                                tweet_text: total_msg_tweet
-                            });
+
+                            var file_path = `./public/media/${code}_${numVid}.mp4`;
+                            TweetVideo(file_path, total_msg_tweet);
+
+                            // var videoTweet = new VideoTweet({
+                            //     file_path: `./public/media/${code}_${numVid}.mp4`,
+                            //     tweet_text: total_msg_tweet
+                            // });
+
                         }
 
                     });
@@ -965,202 +987,106 @@ function TweetMSG(status) {
 
 }
 
-//FUNCTION TWEET VIDEO
-
-var MEDIA_ENDPOINT_URL = 'https://upload.twitter.com/1.1/media/upload.json';
-var POST_TWEET_URL = 'https://api.twitter.com/1.1/statuses/update.json';
-var secret = require('./oauth'); //LENAYK (old : oauth.json)
-
-var OAUTH = secret;
 
 
-/**
- * Video Tweet constructor
- **/
-var VideoTweet = function (data) {
+function TweetVideo(file_path, total_msg_tweet) {
 
-    var self = this;
-    self.file_path = data.file_path;
-    self.tweet_text = data.tweet_text;
-    self.total_bytes = undefined;
-    self.media_id = undefined;
-    self.processing_info = undefined;
+    //FUNCTION TWEET VIDEO
+    var secret = require('auth'); //(lenayk : auth_lena.json) (main : oauth.json)
 
-    // retreives file info and inits upload on complete
-    fs.stat(self.file_path, function (error, stats) {
-        self.total_bytes = stats.size
-        self.upload_init();
-    });
-};
+    var client = new TwitterPackage(secret);
+    var mediaType = 'video/mp4';//'image/gif'; // `'video/mp4'` is also supported
+    const mediaData = require('fs').readFileSync(file_path);
+    const mediaSize = require('fs').statSync(file_path).size;
 
-
-/**
- * Inits media upload
- */
-VideoTweet.prototype.upload_init = function () {
-
-    console.log('INIT');
-
-    var self = this;
-
-    form_data = {
-        'command': 'INIT',
-        'media_type': 'video/mp4',
-        'total_bytes': self.total_bytes,
-        'media_category': 'tweetvideo'
-    }
-
-    // inits media upload
-    request.post({ url: MEDIA_ENDPOINT_URL, oauth: OAUTH, formData: form_data }, function (error, response, body) {
-
-        data = JSON.parse(body)
-
-        // store media ID for later reference
-        self.media_id = data.media_id_string;
-
-        // start appening media segments
-        self.upload_append();
-    });
-}
-
-
-/**
- * Uploads/appends video file segments
- */
-VideoTweet.prototype.upload_append = function () {
-
-    var buffer_length = 5000000;
-    var buffer = new Buffer(buffer_length);
-    var bytes_sent = 0;
-
-    var self = this;
-
-    // open and read video file
-    fs.open(self.file_path, 'r', function (error, file_data) {
-
-        var bytes_read, data,
-            segment_index = 0,
-            segments_completed = 0;
-
-        // upload video file in chunks
-        while (bytes_sent < self.total_bytes) {
-
-            console.log('APPEND');
-
-            bytes_read = fs.readSync(file_data, buffer, 0, buffer_length, null);
-            data = bytes_read < buffer_length ? buffer.slice(0, bytes_read) : buffer;
-
-            var form_data = {
-                command: 'APPEND',
-                media_id: self.media_id,
-                segment_index: segment_index,
-                media_data: data.toString('base64')
-            };
-
-            request.post({ url: MEDIA_ENDPOINT_URL, oauth: OAUTH, formData: form_data }, function () {
-                segments_completed = segments_completed + 1;
-
-                console.log('segment_completed');
-                if (segments_completed == segment_index) {
-                    console.log('Upload chunks complete');
-                    self.upload_finalize();
+    console.log(mediaSize);
+    initUpload() // Declare that you wish to upload some media
+        .then(appendUpload) // Send the data for the media
+        .then(finalizeUpload) // Declare that you are done uploading chunks
+        .then(mediaId => {
+            console.log("Media ID : " + mediaId);
+            // You now have an uploaded movie/animated gif
+            // that you can reference in Tweets, e.g. `update/statuses`
+            // will take a `mediaIds` param.
+            var status = {
+                status: total_msg_tweet,
+                media_ids: mediaId // Pass the media id string
+            }
+            console.log("Start Tweet");
+            client.post('statuses/update', status, function (error, tweet, response) {
+                if (!error) {
+                    console.log(tweet);
                 }
             });
 
-            bytes_sent = bytes_sent + buffer_length;
-            segment_index = segment_index + 1;
-        }
-    });
+
+
+        });
+
+    /**
+* Step 1 of 3: Initialize a media upload
+* @return Promise resolving to String mediaId
+*/
+    function initUpload() {
+        return makePost('media/upload', {
+            command: 'INIT',
+            total_bytes: mediaSize,
+            media_type: mediaType,
+        }).then(data => data.media_id_string);
+    }
+
+    /**
+     * Step 2 of 3: Append file chunk
+     * @param String mediaId    Reference to media object being uploaded
+     * @return Promise resolving to String mediaId (for chaining)
+     */
+    function appendUpload(mediaId) {
+        return makePost('media/upload', {
+            command: 'APPEND',
+            media_id: mediaId,
+            media: mediaData,
+            segment_index: 0
+        }).then(data => mediaId);
+    }
+
+    /**
+     * Step 3 of 3: Finalize upload
+     * @param String mediaId   Reference to media
+     * @return Promise resolving to mediaId (for chaining)
+     */
+    function finalizeUpload(mediaId) {
+        return makePost('media/upload', {
+            command: 'FINALIZE',
+            media_id: mediaId
+        }).then(data => mediaId);
+    }
+
+    /**
+     * (Utility function) Send a POST request to the Twitter API
+     * @param String endpoint  e.g. 'statuses/upload'
+     * @param Object params    Params object to send
+     * @return Promise         Rejects if response is error
+     */
+    function makePost(endpoint, params) {
+        return new Promise((resolve, reject) => {
+            client.post(endpoint, params, (error, data, response) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(data);
+                }
+            });
+        });
+    }
+
+
+
 
 }
 
 
-/**
- * Finalizes media segments uploaded 
- */
-VideoTweet.prototype.upload_finalize = function () {
-
-    console.log('FINALIZE');
-
-    var self = this;
-
-    form_data = {
-        'command': 'FINALIZE',
-        'media_id': self.media_id
-    }
-
-    // finalize uploaded chunck and check processing status on compelete
-    request.post({ url: MEDIA_ENDPOINT_URL, oauth: OAUTH, formData: form_data }, function (error, response, body) {
-
-        data = JSON.parse(body)
-        self.check_status(data.processing_info);
-    });
-}
 
 
-/**
- * Checks status of uploaded media
- */
-VideoTweet.prototype.check_status = function (processing_info) {
-
-    var self = this;
-
-    // if response does not contain any processing_info, then video is ready
-    if (!processing_info) {
-        self.tweet();
-        return;
-    }
-
-    console.log('STATUS');
-
-    request_params = {
-        'command': 'STATUS',
-        'media_id': self.media_id
-    }
-
-    // check processing status 
-    request.get({ url: MEDIA_ENDPOINT_URL, oauth: OAUTH, qs: request_params }, function (error, response, body) {
-
-        data = JSON.parse(body)
-
-        console.log('Media processing status is ' + processing_info.state);
-
-        if (processing_info.state == 'succeeded') {
-            self.tweet();
-            return
-        }
-
-        else if (processing_info.state == 'failed') {
-            return;
-        }
-
-        // check status again after specified duration
-        var timeout_length = data.processing_info.check_after_secs ? data.processing_info.check_after_secs * 1000 : 0;
-
-        console.log('Checking after ' + timeout_length + ' milliseconds');
-
-        setTimeout(function () {
-            self.check_status(data.processing_info)
-        }, timeout_length);
-    });
-}
 
 
-/**
- * Tweets text with attached media
- */
-VideoTweet.prototype.tweet = function () {
 
-    var self = this;
-
-    request_data = {
-        'status': self.tweet_text,
-        'media_ids': self.media_id
-    }
-
-    // publish Tweet
-    request.post({ url: POST_TWEET_URL, oauth: OAUTH, form: request_data }, function (error, response, body) {
-        data = JSON.parse(body)
-        console.log(data);
-    });
-}
